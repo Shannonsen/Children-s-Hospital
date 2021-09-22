eb69143e7230e51989befba0d9986dc682dbd8f7
Shanna <A18016307@alumnos.uady.mx>
2021-09-22 10:38:23 -0500

    add class 'Patient' and 'Date'

--- Hospital/src/Data/Patient.java
+++ Hospital/src/Data/Patient.java
@@ -11,5 +11,86 @@ package Data;
  * @author Shannon
  */
 public class Patient {
+    String name; //Complete name
+    String age;
+    String gender;
+    Date dateBirth;
+    String originCity;
+    Date dateInscription;
+    String originHospital;
+    String tutorName;
+    String telephone;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    public String getOriginCity() {
+        return originCity;
+    }
+
+    public Date getDateInscription() {
+        return dateInscription;
+    }
+
+    public String getOriginHospital() {
+        return originHospital;
+    }
+
+    public String getTutorName() {
+        return tutorName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public void setOriginCity(String originCity) {
+        this.originCity = originCity;
+    }
+
+    public void setDateInscription(Date dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+
+    public void setOriginHospital(String originHospital) {
+        this.originHospital = originHospital;
+    }
+
+    public void setTutorName(String tutorName) {
+        this.tutorName = tutorName;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
     
 }
